b25bc872e29552a981ad80527509e15c308d3bf3
kunal-hash <kunalpardhi7319@gmail.com>
2023-03-25 03:28:21 +0530

    final

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,12 @@
-// write code for Welcome component here
+import React from "react";
+function Welcome(props) {
+  return (
+    <React.Fragment>
+      <p>
+        Hey ! <h1>{props.name}</h1>
+      </p>
+      <h2>Welcome to school.</h2>
+    </React.Fragment>
+  );
+}
+export default Welcome;
